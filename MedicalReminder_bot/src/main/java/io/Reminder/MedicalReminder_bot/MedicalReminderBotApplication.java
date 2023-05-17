ae8f54826b6c9465b56ae02dd31a38bf0929cd7c
VenziLio <89646230+VenziLio@users.noreply.github.com>
2023-05-17 10:55:25 +0300

    Add files via upload

---
+++ MedicalReminder_bot/src/main/java/io/Reminder/MedicalReminder_bot/MedicalReminderBotApplication.java
@@ -0,0 +1,13 @@
+package io.Reminder.MedicalReminder_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalReminderBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalReminderBotApplication.class, args);
+	}
+
+}
---
+++ MedicalReminder_bot/src/test/java/io/Reminder/MedicalReminder_bot/MedicalReminderBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.Reminder.MedicalReminder_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalReminderBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
